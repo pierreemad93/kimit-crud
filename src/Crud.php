a87f8b9021e54854f439cc4c4e4b62d1b82033a8
pierreemad93 <pierreemad93@outlook.com>
2024-12-17 20:39:42 +0200

    first commit

---
+++ src/Crud.php
@@ -0,0 +1,25 @@
+<?php
+use PDO ;
+abstract class CRUD
+{
+   protected $connection;
+   protected $query;
+   protected $stmt;
+   public function __construct($dsn, $username, $password)
+   {
+      $this->connection = new PDO($dsn, $username, $password);
+   }
+   abstract public function index(string $table, string|array $columns = "*");
+   abstract public function store();
+   public function execute()
+   {
+      $this->stmt->execute();
+      return $this;
+   }
+
+   public function get()
+   {
+      $data = $this->stmt->fetchAll();
+      print_r($data);
+   }
+}
